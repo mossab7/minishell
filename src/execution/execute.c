99f39a467ae90c6e0d4daf0d2a4d980e10190abc
Mossaab Bouhia <mbouhia@c4r10p9.1337.ma>
2025-02-14 20:47:13 +0100

    norm edits

--- src/execution/execute.c
+++ src/execution/execute.c
@@ -3,216 +3,248 @@
 #include <sys/wait.h>
 #include <termios.h>
 
-int setup_redirections(t_command *cmd, int *saved_fds)
+int	setup_redirections(t_command *cmd, int *saved_fds)
 {
-    saved_fds[0] = dup(STDIN_FILENO);
-    saved_fds[1] = dup(STDOUT_FILENO);
+	t_redirect	*redir;
+		int fd;
+			char template[] = "/tmp/heredoc_XXXXXX";
+			char *line;
 
-    for (int i = 0; i < cmd->redirect_count; i++) {
-        t_redirect *redir = cmd->redirects[i];
-        int fd;
-
-        switch (redir->type) {
-            case REDIR_INPUT:
-                fd = open(redir->filename, O_RDONLY);
-                if (fd == -1) {
-                    perror(redir->filename);
-                    return -1;
-                }
-                dup2(fd, STDIN_FILENO);
-                close(fd);
-                break;
-
-            case REDIR_OUTPUT:
-                fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (fd == -1) {
-                    perror(redir->filename);
-                    return -1;
-                }
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
-                break;
-
-            case REDIR_APPEND:
-                fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-                if (fd == -1) {
-                    perror(redir->filename);
-                    return -1;
-                }
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
-                break;
-
-            case REDIR_HEREDOC:
-                char template[] = "/tmp/heredoc_XXXXXX";
-                fd = mkstemp(template);
-                if (fd == -1) {
-                    perror("heredoc");
-                    return -1;
-                }
-                unlink(template);
-
-                char *line;
-                while ((line = readline("> ")) != NULL) {
-                    if (strcmp(line, redir->delimiter) == 0) {
-                        free(line);
-                        break;
-                    }
-                    write(fd, line, strlen(line));
-                    write(fd, "\n", 1);
-                    free(line);
-                }
-
-                lseek(fd, 0, SEEK_SET);
-                dup2(fd, STDIN_FILENO);
-                close(fd);
-                break;
-        }
-    }
-    return 0;
+	saved_fds[0] = dup(STDIN_FILENO);
+	saved_fds[1] = dup(STDOUT_FILENO);
+	for (int i = 0; i < cmd->redirect_count; i++)
+	{
+		redir = cmd->redirects[i];
+		switch (redir->type)
+		{
+		case REDIR_INPUT:
+			fd = open(redir->filename, O_RDONLY);
+			if (fd == -1)
+			{
+				perror(redir->filename);
+				return (-1);
+			}
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+			break ;
+		case REDIR_OUTPUT:
+			fd = open(redir->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (fd == -1)
+			{
+				perror(redir->filename);
+				return (-1);
+			}
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+			break ;
+		case REDIR_APPEND:
+			fd = open(redir->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			if (fd == -1)
+			{
+				perror(redir->filename);
+				return (-1);
+			}
+			dup2(fd, STDOUT_FILENO);
+			close(fd);
+			break ;
+		case REDIR_HEREDOC:
+			fd = mkstemp(template);
+			if (fd == -1)
+			{
+				perror("heredoc");
+				return (-1);
+			}
+			unlink(template);
+			while ((line = readline("> ")) != NULL)
+			{
+				if (strcmp(line, redir->delimiter) == 0)
+				{
+					free(line);
+					break ;
+				}
+				write(fd, line, strlen(line));
+				write(fd, "\n", 1);
+				free(line);
+			}
+			lseek(fd, 0, SEEK_SET);
+			dup2(fd, STDIN_FILENO);
+			close(fd);
+			break ;
+		}
+	}
+	return (0);
 }
 
-int execute_command(t_command *cmd, t_env *env)
-{
-    int saved_fds[2];
-    int status = 0;
+typedef int	(*built_in_command)(t_env *, char **args);
 
-    if (cmd->argc == 0)
-		return 0;
-    pid_t current_pid = fork();
+int	execute_built_in_commands(char *command, t_env *env, char **args)
+{
+	built_in_command	functions[] = {built_in_cd, built_in_echo, built_in_env,
+			built_in_exit, built_in_export, NULL};
+	char				*function_names[] = {"cd", "echo", "env", "exit",
+						"export", NULL};
+	int					i;
+
+	i = 0;
+	while (function_names[i])
+	{
+		if (ft_strcmp(function_names[i], command) == 0)
+			break ;
+		i++;
+	}
+	if (functions[i])
+		return (functions[i](env, args));
+	return (1);
+}
+
+int	execute_command(t_command *cmd, t_env *env)
+{
+	int			saved_fds[2];
+	int			status;
+	pid_t		current_pid;
+	t_string	*cmd_path;
+
+	status = 0;
+	if (cmd->argc == 0)
+		return (0);
+	if (!execute_built_in_commands(cmd->args[0], env, cmd->args))
+		return (0);
+	current_pid = fork();
 	if (current_pid == -1)
 	{
-        perror("fork");
-        return -1;
-    }
-    if (current_pid == 0)
+		perror("fork");
+		return (-1);
+	}
+	if (current_pid == 0)
 	{
-        if (setup_redirections(cmd, saved_fds) == -1)
-            exit(EXIT_FAILURE);
-        t_string *cmd_path = search_path(env->path, cmd->args[0]);
+		if (setup_redirections(cmd, saved_fds) == -1)
+			exit(EXIT_FAILURE);
+		cmd_path = search_path(env->path, cmd->args[0]);
 		if (cmd_path)
 		{
-            execve(cmd_path->cstring, cmd->args, env->envp);
-            str_destruct(cmd_path);
-        }
-        ft_printf("minishell: %s: command not found\n", cmd->args[0]);
-        exit(127);
-    }
-
-    waitpid(current_pid, &status, 0);
-    if (WIFSIGNALED(status)) {
-        env->last_command_status = 128 + WTERMSIG(status);
-    } else {
-        env->last_command_status = WEXITSTATUS(status);
-    }
-
-    return (env->last_command_status);
+			execve(cmd_path->cstring, cmd->args, env->envp);
+			str_destruct(cmd_path);
+		}
+		ft_printf("minishell: %s: command not found\n", cmd->args[0]);
+		exit(127);
+	}
+	waitpid(current_pid, &status, 0);
+	if (WIFSIGNALED(status))
+	{
+		env->last_command_status = 128 + WTERMSIG(status);
+	}
+	else
+	{
+		env->last_command_status = WEXITSTATUS(status);
+	}
+	return (env->last_command_status);
 }
 
-int execute_pipe(t_ast *node, t_env *env) {
-    int pipefd[2];
-    int status;
-    pid_t pid1, pid2;
-
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        return -1;
-    }
-
-    pid1 = fork();
-    if (pid1 == -1) {
-        perror("fork");
-        close(pipefd[0]);
-        close(pipefd[1]);
-        return -1;
-    }
-
-    if (pid1 == 0) {
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[1]);
-        exit(execute_ast(node->left, env));
-    }
-
-    pid2 = fork();
-    if (pid2 == -1) {
-        perror("fork");
-        close(pipefd[0]);
-        close(pipefd[1]);
-        return -1;
-    }
-
-    if (pid2 == 0) {
-        close(pipefd[1]);
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[0]);
-        exit(execute_ast(node->right, env));
-    }
-
-    close(pipefd[0]);
-    close(pipefd[1]);
-
-    waitpid(pid1, &status, 0);
-    int pipe_status;
-    waitpid(pid2, &pipe_status, 0);
-
-    env->last_command_status = WEXITSTATUS(pipe_status);
-    return env->last_command_status;
-}
-
-int execute_subshell(t_ast *node, t_env *env)
+int	execute_pipe(t_ast *node, t_env *env)
 {
-    pid_t pid = fork();
-    if (pid == -1)
-	{
-        perror("fork");
-        return -1;
-    }
+	int	pipefd[2];
+	int	status;
+	int	pipe_status;
 
-    if (pid == 0)
+	pid_t pid1, pid2;
+	if (pipe(pipefd) == -1)
 	{
-        t_env *subshell_env = env_copy(env);
-        int result = execute_ast(node->left, subshell_env);
-        env_destroy(subshell_env);
-        exit(result);
-    }
-
-    int status;
-    waitpid(pid, &status, 0);
-    env->last_command_status = WEXITSTATUS(status);
-    return env->last_command_status;
+		perror("pipe");
+		return (-1);
+	}
+	pid1 = fork();
+	if (pid1 == -1)
+	{
+		perror("fork");
+		close(pipefd[0]);
+		close(pipefd[1]);
+		return (-1);
+	}
+	if (pid1 == 0)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+		exit(execute_ast(node->left, env));
+	}
+	pid2 = fork();
+	if (pid2 == -1)
+	{
+		perror("fork");
+		close(pipefd[0]);
+		close(pipefd[1]);
+		return (-1);
+	}
+	if (pid2 == 0)
+	{
+		close(pipefd[1]);
+		dup2(pipefd[0], STDIN_FILENO);
+		close(pipefd[0]);
+		exit(execute_ast(node->right, env));
+	}
+	close(pipefd[0]);
+	close(pipefd[1]);
+	waitpid(pid1, &status, 0);
+	waitpid(pid2, &pipe_status, 0);
+	env->last_command_status = WEXITSTATUS(pipe_status);
+	return (env->last_command_status);
 }
 
-int execute_ast(t_ast *node, t_env *env) {
-    if (!node) return 0;
+int	execute_subshell(t_ast *node, t_env *env)
+{
+	pid_t	pid;
+	t_env	*subshell_env;
+	int		result;
+	int		status;
 
-    switch (node->type) {
-        case NODE_COMMAND:
-            return execute_command(&node->value.command, env);
-
-        case NODE_PIPE:
-            return execute_pipe(node, env);
-
-        case NODE_SUBSHELL:
-            return execute_subshell(node, env);
-
-        case NODE_LOGICAL_AND: {
-            int left_status = execute_ast(node->left, env);
-            if (left_status == 0)
-                return execute_ast(node->right, env);
-            return left_status;
-        }
-
-        case NODE_LOGICAL_OR: {
-            int left_status = execute_ast(node->left, env);
-            if (left_status != 0)
-                return execute_ast(node->right, env);
-            return left_status;
-        }
-
-        default:
-            ft_printf("Unknown node type\n");
-            return -1;
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (-1);
+	}
+	if (pid == 0)
+	{
+		subshell_env = env_copy(env);
+		result = execute_ast(node->left, subshell_env);
+		env_destroy(subshell_env);
+		exit(result);
+	}
+	waitpid(pid, &status, 0);
+	env->last_command_status = WEXITSTATUS(status);
+	return (env->last_command_status);
 }
 
+int	execute_ast(t_ast *node, t_env *env)
+{
+	int	left_status;
+
+	if (!node)
+		return (0);
+	switch (node->type)
+	{
+	case NODE_COMMAND:
+		return (execute_command(&node->value.command, env));
+	case NODE_PIPE:
+		return (execute_pipe(node, env));
+	case NODE_SUBSHELL:
+		return execute_subshell(node, env);
+	case NODE_LOGICAL_AND:
+	{
+		left_status = execute_ast(node->left, env);
+		if (left_status == 0)
+			return execute_ast(node->right, env);
+		return left_status;
+	}
+	case NODE_LOGICAL_OR:
+	{
+		left_status = execute_ast(node->left, env);
+		if (left_status != 0)
+			return execute_ast(node->right, env);
+		return left_status;
+	}
+	default:
+		ft_printf("Unknown node type\n");
+		return -1;
+	}
+}
--- src/parsing/syntax_tree.c
+++ src/parsing/syntax_tree.c
@@ -1,235 +1,309 @@
-#include <zen.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <zen.h>
-void print_ast(t_ast *node, int level) {
-    if (node == NULL) {
-        return;
-    }
 
-    for (int i = 0; i < level; i++) {
-        printf("    ");
-    }
-
-    switch (node->type) {
-        case NODE_COMMAND: {
-            printf("├── COMMAND\n");
-            for (int i = 0; i < level + 1; i++) {
-                printf("    ");
-            }
-            printf("├── ARGS: ");
-            for (int i = 0; i < node->value.command.argc; i++) {
-                printf("%s ", node->value.command.args[i]);
-            }
-            printf("\n");
-
-            for (int i = 0; i < level + 1; i++) {
-                printf("    ");
-            }
-            printf("├── REDIRECTS: ");
-            for (int i = 0; i < node->value.command.redirect_count; i++) {
-                t_redirect *redir = node->value.command.redirects[i];
-                if (redir->type == REDIR_INPUT) {
-                    printf("< %s ", redir->filename);
-                } else if (redir->type == REDIR_OUTPUT) {
-                    printf("> %s ", redir->filename);
-                } else if (redir->type == REDIR_APPEND) {
-                    printf(">> %s ", redir->filename);
-                } else if (redir->type == REDIR_HEREDOC) {
-                    printf("<< %s ", redir->delimiter);
-                }
-            }
-            printf("\n");
-            break;
-        }
-        case NODE_PIPE:
-            printf("├── PIPE\n");
-            break;
-        case NODE_SUBSHELL:
-            printf("├── SUBSHELL\n");
-            break;
-        case NODE_LOGICAL_AND:
-            printf("├── AND\n");
-            break;
-        case NODE_LOGICAL_OR:
-            printf("├── OR\n");
-            break;
-        default:
-            printf("├── UNKNOWN NODE TYPE\n");
-            break;
-    }
-
-    if (node->left) {
-        print_ast(node->left, level + 1);
-    }
-    if (node->right) {
-        print_ast(node->right, level + 1);
-    }
-}
-
-
-t_ast *parse_and_or(t_token_array *tokens, size_t *index);
-
-void syntax_error(const char *message) {
-    fprintf(stderr, "Syntax error: %s\n", message);
-    exit(EXIT_FAILURE);
-}
-
-t_ast *create_command_node(char **args, int argc, t_redirect **redirects, int redirect_count) {
-    t_ast *node = alloc(sizeof(t_ast));
-    node->type = NODE_COMMAND;
-    node->value.command = (t_command){
-        .args = args,
-        .argc = argc,
-        .redirects = redirects,
-        .redirect_count = redirect_count
-    };
-    return node;
-}
-
-t_ast *create_binary_node(t_node_type type, t_ast *left, t_ast *right) {
-    t_ast *node = alloc(sizeof(t_ast));
-    node->type = type;
-    node->left = left;
-    node->right = right;
-    return node;
-}
-
-t_ast *create_subshell_node(t_ast *child) {
-    t_ast *node = alloc(sizeof(t_ast));
-    node->type = NODE_SUBSHELL;
-    node->left = child;
-    node->right = NULL;
-    return node;
-}
-t_redirect *create_redirect(t_redirect_type type, char *target) {
-    t_redirect *redir = alloc(sizeof(t_redirect));
-    redir->type = type;
-    if (type == REDIR_HEREDOC) {
-        redir->delimiter = target;
-        redir->filename = NULL;
-    } else {
-        redir->filename = target;
-        redir->delimiter = NULL;
-    }
-    return redir;
-}
-
-t_token peek_token(t_token_array *tokens, size_t index) {
-    if (index >= tokens->size) return (t_token){TOK_EOF, NULL};
-    return tokens->items[index];
-}
-
-bool match_token(t_token_type type, t_token_array *tokens, size_t *index) {
-    if (*index >= tokens->size) return false;
-    if (tokens->items[*index].type == type) {
-        (*index)++;
-        return true;
-    }
-    return false;
-}
-
-t_redirect *parse_redirection(t_token_array *tokens, size_t *index)
+void	print_ast(t_ast *node, int level)
 {
-	printf("hola\n");
-    t_redirect_type type;
-    t_token token = peek_token(tokens, *index);
+	t_redirect	*redir;
 
-    if (token.type == TOK_INPUT_REDIRECT) {
-        type = REDIR_INPUT;
-    } else if (token.type == TOK_OUTPUT_REDIRECT) {
-        type = REDIR_OUTPUT;
-    } else if (token.type == TOK_APPEND) {
-        type = REDIR_APPEND;
-    } else if (token.type == TOK_HEREDOC) {
-        type = REDIR_HEREDOC;
-    } else {
-        return NULL;
-    }
-    (*index)++;
-
-    token = peek_token(tokens, *index);
-    if (token.type != TOK_WORD) syntax_error("Expected filename/delimiter after redirection");
-    (*index)++;
-
-    return create_redirect(type, ft_strdup(token.lexeme->cstring));
-}
-
-t_ast *parse_command(t_token_array *tokens, size_t *index)
-{
-    char **args = alloc((tokens->size + 1) * sizeof(char *));
-    t_redirect **redirects = alloc(tokens->size * sizeof(t_redirect *));
-    int argc = 0, redirect_count = 0;
-
-    while (true) {
-        t_token token = peek_token(tokens, *index);
-        if (token.type == TOK_WORD) {
-            args[argc++] = ft_strdup(token.lexeme->cstring);
-            (*index)++;
-        }
-        else if (token.type >= TOK_INPUT_REDIRECT && token.type <= TOK_HEREDOC) {
-            t_redirect *redir = parse_redirection(tokens, index);
-            redirects[redirect_count++] = redir;
-        }
-        else break;
-    }
-
-    args = realloc(args, (argc + 1) * sizeof(char *));
-    redirects = realloc(redirects, redirect_count * sizeof(t_redirect *));
-    return create_command_node(args, argc, redirects, redirect_count);
-}
-
-
-
-t_ast *parse_primary(t_token_array *tokens, size_t *index) {
-    t_ast *node = NULL;
-
-    if (peek_token(tokens, *index).type == TOK_OPAREN) {
-        (*index)++;
-        node = parse_and_or(tokens, index);
-        if (!match_token(TOK_CPAREN, tokens, index)) {
-            syntax_error("Expected closing parenthesis");
-        }
-        node = create_subshell_node(node);
-    } else {
-        node = parse_command(tokens, index);
-    }
-
-    return node;
-}
-
-t_ast *parse_pipe(t_token_array *tokens, size_t *index) {
-    t_ast *left = parse_primary(tokens, index);
-    while (match_token(TOK_PIPE, tokens, index)) {
-        left = create_binary_node(NODE_PIPE, left, parse_primary(tokens, index));
-    }
-    return left;
-}
-
-t_ast *parse_and_or(t_token_array *tokens, size_t *index) {
-    t_ast *left = parse_pipe(tokens, index);
-    while (true) {
-        if (match_token(TOK_LOGICAL_AND, tokens, index)) {
-            left = create_binary_node(NODE_LOGICAL_AND, left, parse_pipe(tokens, index));
-        } else if (match_token(TOK_LOGICAL_OR, tokens, index)) {
-            left = create_binary_node(NODE_LOGICAL_OR, left, parse_pipe(tokens, index));
-        } else break;
-    }
-    return left;
-}
-
-t_ast *build_ast(t_token_array *tokens) {
-    size_t index = 0;
-    t_ast *ast = parse_and_or(tokens, &index);
-
-    t_token token = peek_token(tokens, index);
-    if (token.type != TOK_EOF)
+	if (node == NULL)
 	{
-		printf("%s\n",token.lexeme->cstring);
+		return ;
+	}
+	for (int i = 0; i < level; i++)
+	{
+		printf("    ");
+	}
+	switch (node->type)
+	{
+	case NODE_COMMAND:
+	{
+		printf("├── COMMAND\n");
+		for (int i = 0; i < level + 1; i++)
+		{
+			printf("    ");
+		}
+		printf("├── ARGS: ");
+		for (int i = 0; i < node->value.command.argc; i++)
+		{
+			printf("%s ", node->value.command.args[i]);
+		}
+		printf("\n");
+		for (int i = 0; i < level + 1; i++)
+		{
+			printf("    ");
+		}
+		printf("├── REDIRECTS: ");
+		for (int i = 0; i < node->value.command.redirect_count; i++)
+		{
+			redir = node->value.command.redirects[i];
+			if (redir->type == REDIR_INPUT)
+			{
+				printf("< %s ", redir->filename);
+			}
+			else if (redir->type == REDIR_OUTPUT)
+			{
+				printf("> %s ", redir->filename);
+			}
+			else if (redir->type == REDIR_APPEND)
+			{
+				printf(">> %s ", redir->filename);
+			}
+			else if (redir->type == REDIR_HEREDOC)
+			{
+				printf("<< %s ", redir->delimiter);
+			}
+		}
+		printf("\n");
+		break ;
+	}
+	case NODE_PIPE:
+		printf("├── PIPE\n");
+		break ;
+	case NODE_SUBSHELL:
+		printf("├── SUBSHELL\n");
+		break ;
+	case NODE_LOGICAL_AND:
+		printf("├── AND\n");
+		break ;
+	case NODE_LOGICAL_OR:
+		printf("├── OR\n");
+		break ;
+	default:
+		printf("├── UNKNOWN NODE TYPE\n");
+		break ;
+	}
+	if (node->left)
+	{
+		print_ast(node->left, level + 1);
+	}
+	if (node->right)
+	{
+		print_ast(node->right, level + 1);
+	}
+}
+
+t_ast		*parse_and_or(t_token_array *tokens, size_t *index);
+
+void	syntax_error(const char *message)
+{
+	fprintf(stderr, "Syntax error: %s\n", message);
+	exit(EXIT_FAILURE);
+}
+
+t_ast	*create_command_node(char **args, int argc, t_redirect **redirects,
+		int redirect_count)
+{
+	t_ast	*node;
+
+	node = alloc(sizeof(t_ast));
+	node->type = NODE_COMMAND;
+	node->value.command = (t_command){.args = args, .argc = argc,
+		.redirects = redirects, .redirect_count = redirect_count};
+	return (node);
+}
+
+t_ast	*create_binary_node(t_node_type type, t_ast *left, t_ast *right)
+{
+	t_ast	*node;
+
+	node = alloc(sizeof(t_ast));
+	node->type = type;
+	node->left = left;
+	node->right = right;
+	return (node);
+}
+
+t_ast	*create_subshell_node(t_ast *child)
+{
+	t_ast	*node;
+
+	node = alloc(sizeof(t_ast));
+	node->type = NODE_SUBSHELL;
+	node->left = child;
+	node->right = NULL;
+	return (node);
+}
+
+t_redirect	*create_redirect(t_redirect_type type, char *target)
+{
+	t_redirect	*redir;
+
+	redir = alloc(sizeof(t_redirect));
+	redir->type = type;
+	if (type == REDIR_HEREDOC)
+	{
+		redir->delimiter = target;
+		redir->filename = NULL;
+	}
+	else
+	{
+		redir->filename = target;
+		redir->delimiter = NULL;
+	}
+	return (redir);
+}
+
+t_token	peek_token(t_token_array *tokens, size_t index)
+{
+	if (index >= tokens->size)
+		return ((t_token){TOK_EOF, NULL});
+	return (tokens->items[index]);
+}
+
+bool	match_token(t_token_type type, t_token_array *tokens, size_t *index)
+{
+	if (*index >= tokens->size)
+		return (false);
+	if (tokens->items[*index].type == type)
+	{
+		(*index)++;
+		return (true);
+	}
+	return (false);
+}
+
+t_redirect	*parse_redirection(t_token_array *tokens, size_t *index)
+{
+	t_redirect_type	type;
+	t_token			token;
+
+	printf("hola\n");
+	token = peek_token(tokens, *index);
+	if (token.type == TOK_INPUT_REDIRECT)
+	{
+		type = REDIR_INPUT;
+	}
+	else if (token.type == TOK_OUTPUT_REDIRECT)
+	{
+		type = REDIR_OUTPUT;
+	}
+	else if (token.type == TOK_APPEND)
+	{
+		type = REDIR_APPEND;
+	}
+	else if (token.type == TOK_HEREDOC)
+	{
+		type = REDIR_HEREDOC;
+	}
+	else
+	{
+		return (NULL);
+	}
+	(*index)++;
+	token = peek_token(tokens, *index);
+	if (token.type != TOK_WORD)
+		syntax_error("Expected filename/delimiter after redirection");
+	(*index)++;
+	return (create_redirect(type, ft_strdup(token.lexeme->cstring)));
+}
+
+t_ast	*parse_command(t_token_array *tokens, size_t *index)
+{
+	char		**args;
+	t_redirect	**redirects;
+	int			argc = 0, redirect_count;
+	t_token		token;
+	t_redirect	*redir;
+
+	args = alloc((tokens->size + 1) * sizeof(char *));
+	redirects = alloc(tokens->size * sizeof(t_redirect *));
+	argc = 0, redirect_count = 0;
+	while (true)
+	{
+		token = peek_token(tokens, *index);
+		if (token.type == TOK_WORD)
+		{
+			args[argc++] = ft_strdup(token.lexeme->cstring);
+			(*index)++;
+		}
+		else if (token.type >= TOK_INPUT_REDIRECT && token.type <= TOK_HEREDOC)
+		{
+			redir = parse_redirection(tokens, index);
+			redirects[redirect_count++] = redir;
+		}
+		else
+			break ;
+	}
+	args = realloc(args, (argc + 1) * sizeof(char *));
+	redirects = realloc(redirects, redirect_count * sizeof(t_redirect *));
+	return (create_command_node(args, argc, redirects, redirect_count));
+}
+
+t_ast	*parse_primary(t_token_array *tokens, size_t *index)
+{
+	t_ast	*node;
+
+	node = NULL;
+	if (peek_token(tokens, *index).type == TOK_OPAREN)
+	{
+		(*index)++;
+		node = parse_and_or(tokens, index);
+		if (!match_token(TOK_CPAREN, tokens, index))
+		{
+			syntax_error("Expected closing parenthesis");
+		}
+		node = create_subshell_node(node);
+	}
+	else
+	{
+		node = parse_command(tokens, index);
+	}
+	return (node);
+}
+
+t_ast	*parse_pipe(t_token_array *tokens, size_t *index)
+{
+	t_ast	*left;
+
+	left = parse_primary(tokens, index);
+	while (match_token(TOK_PIPE, tokens, index))
+	{
+		left = create_binary_node(NODE_PIPE, left, parse_primary(tokens,
+					index));
+	}
+	return (left);
+}
+
+t_ast	*parse_and_or(t_token_array *tokens, size_t *index)
+{
+	t_ast	*left;
+
+	left = parse_pipe(tokens, index);
+	while (true)
+	{
+		if (match_token(TOK_LOGICAL_AND, tokens, index))
+		{
+			left = create_binary_node(NODE_LOGICAL_AND, left, parse_pipe(tokens,
+						index));
+		}
+		else if (match_token(TOK_LOGICAL_OR, tokens, index))
+		{
+			left = create_binary_node(NODE_LOGICAL_OR, left, parse_pipe(tokens,
+						index));
+		}
+		else
+			break ;
+	}
+	return (left);
+}
+
+t_ast	*build_ast(t_token_array *tokens)
+{
+	size_t	index;
+	t_ast	*ast;
+	t_token	token;
+
+	index = 0;
+	ast = parse_and_or(tokens, &index);
+	token = peek_token(tokens, index);
+	if (token.type != TOK_EOF)
+	{
+		printf("%s\n", token.lexeme->cstring);
 		syntax_error("Unexpected input");
 	}
-    return ast;
+	return (ast);
 }
