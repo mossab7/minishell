59c22c6a1dd8894ce706ef9af98aeccc06422b68
blackstar <blackstar@blackstar.blackstar>
2025-03-25 02:47:59 -0400

    SIGTSTP fixed in signal setup for heredoc

--- src/signals/here_doc_signals.c
+++ src/signals/here_doc_signals.c
@@ -14,9 +14,10 @@ void	setup_heredoc_signals(void)
 	signo = 1;
 	while (signo < NSIG)
 	{
-		if (signo != SIGKILL)
+		if (signo != SIGKILL && signo != SIGSTOP)
 			signal(signo, SIG_IGN);
 		signo++;
 	}
 	signal(SIGINT, handle_child_sigint);
+	signal(SIGTSTP, SIG_IGN);
 }
