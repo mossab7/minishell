559ac88720f041dc3f1791e3ae5ddad00d8db3fc
Lahoucine Azmoud <lazmoud@e2r6p6.1337.ma>
2025-03-25 21:37:16 +0000

    _: I made it so it gets added only to the env cells, not the export cells

--- src/environment/env_insert_defaults.c
+++ src/environment/env_insert_defaults.c
@@ -6,7 +6,7 @@
 /*   By: lazmoud <lazmoud@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 16:03:07 by lazmoud           #+#    #+#             */
-/*   Updated: 2025/03/25 21:28:51 by lazmoud          ###   ########.fr       */
+/*   Updated: 2025/03/25 21:37:10 by lazmoud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <zen.h>
@@ -17,7 +17,11 @@ void	env_set_defaults(t_env *env)
 
 	ft_memset(pwd, 0, PATH_MAX);
 	if (!getcwd(pwd, PATH_MAX))
-		zen_elog("cd: error getting current directory\n");
+	{
+		zen_elog("error retrieving current directory: "
+			"getcwd: cannot access parent directories: "
+			"No such file or directory\n");
+	}
 	insert_if_not_found(env->cells, "PWD", pwd);
 	insert_if_not_found(env->export_cells, "PWD", pwd);
 	insert_if_not_found(env->cells, "SHLVL", "1");
