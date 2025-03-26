ea8f02274bb8c108a2b3699155000a39e6122620
Mossaab Bouhia <mbouhia@c4r10p6.1337.ma>
2025-03-26 15:27:53 +0000

    fixed exit arguments and return value problem

--- src/built_ins/exit.c
+++ src/built_ins/exit.c
@@ -11,6 +11,17 @@
 /* ************************************************************************** */
 #include <zen.h>
 
+static bool	is_numeric(char *str)
+{
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (false);
+		str++;
+	}
+	return (true);
+}
+
 int	built_in_exit(t_env *env, char **args)
 {
 	int	code;
@@ -18,7 +29,20 @@ int	built_in_exit(t_env *env, char **args)
 	(void)env;
 	code = 0;
 	if (args && args[1])
+	{
+		if (args[2])
+		{
+			zen_elog("exit: too many arguments \n");
+			return (1);
+		}
+		else if (!is_numeric(args[1]))
+		{
+			zen_elog("exit: %s: numeric argument required \n", args[1]);
+			return (2);
+		}
 		code = ft_atoi(args[1]);
+	}
+	ft_printf("exit\n");
 	cleanup_memory_tracker(get_memory_tracker());
 	exit(code);
 	return (0);
