2b85cc41f9836507dff889f3dca749d986ce890f
blackstar <blackstar@blackstar.blackstar>
2025-04-16 18:52:01 -0400

    changed wildcard match pattern to take quoated wildcards as part of the pattern

--- src/expansion/wildcard_match.c
+++ src/expansion/wildcard_match.c
@@ -13,24 +13,29 @@
 
 bool	match_pattern(t_string *pattern, const char *str, int index)
 {
-	if (pattern->cstring[index] == '\0')
-		return (*str == '\0');
-	str_print(pattern);
-	if (pattern->cstring[index] != '*')
-	{
-		if (*str == '\0')
-			return (false);
-		if (pattern->cstring[index] != *str)
-			return (false);
-		return (match_pattern(pattern, str + 1, ++index));
-	}
-	while (pattern->cstring[index + 1] == '*' && (pattern->mask->items[index + 1] & NOT_QUOTED))
-		index++;
-	while (*str != '\0')
-	{
-		if (match_pattern(pattern, str, ++index))
-			return (true);
-		str++;
-	}
-	return (match_pattern(pattern, str, ++index));
+    if (pattern->cstring[index] == '\0')
+        return (*str == '\0');
+    
+    if (pattern->cstring[index] == '*' && (pattern->mask->items[index] & NOT_QUOTED))
+    {
+        while (pattern->cstring[index + 1] == '*' && 
+               (pattern->mask->items[index + 1] & NOT_QUOTED))
+            index++;
+        
+        while (true)
+        {
+            if (match_pattern(pattern, str, index + 1))
+                return (true);
+            if (*str == '\0')
+                break;
+            str++;
+        }
+        return (false);
+    }
+    else
+    {
+        if (*str == '\0' || pattern->cstring[index] != *str)
+            return (false);
+        return (match_pattern(pattern, str + 1, index + 1));
+    }
 }
