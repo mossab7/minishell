40c3603eb7c272227e1de3d7cce993cea7ddc50e
Mossaab Bouhia <mossabbouhia@gmail.com>
2025-04-16 20:05:57 +0100

    norm edit

--- src/expansion/wildcard_match.c
+++ src/expansion/wildcard_match.c
@@ -11,31 +11,31 @@
 /* ************************************************************************** */
 #include <zen.h>
 
-bool	match_pattern(t_string *pattern, const char *str, int index)
+bool match_pattern(t_string *pattern, const char *str, int index)
 {
-    if (pattern->cstring[index] == '\0')
-        return (*str == '\0');
-    
-    if (pattern->cstring[index] == '*' && (pattern->mask->items[index] & NOT_QUOTED))
-    {
-        while (pattern->cstring[index + 1] == '*' && 
-               (pattern->mask->items[index + 1] & NOT_QUOTED))
-            index++;
-        
-        while (true)
-        {
-            if (match_pattern(pattern, str, index + 1))
-                return (true);
-            if (*str == '\0')
-                break;
-            str++;
-        }
-        return (false);
-    }
-    else
-    {
-        if (*str == '\0' || pattern->cstring[index] != *str)
-            return (false);
-        return (match_pattern(pattern, str + 1, index + 1));
-    }
+	if (pattern->cstring[index] == '\0')
+		return (*str == '\0');
+
+	if (pattern->cstring[index] == '*' && (pattern->mask->items[index] & NOT_QUOTED))
+	{
+		while (pattern->cstring[index + 1] == '*' &&
+			   (pattern->mask->items[index + 1] & NOT_QUOTED))
+			index++;
+
+		while (true)
+		{
+			if (match_pattern(pattern, str, index + 1))
+				return (true);
+			if (*str == '\0')
+				break;
+			str++;
+		}
+		return (false);
+	}
+	else
+	{
+		if (*str == '\0' || pattern->cstring[index] != *str)
+			return (false);
+		return (match_pattern(pattern, str + 1, index + 1));
+	}
 }
