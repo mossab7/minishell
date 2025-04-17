104ec384970afded136b29a4bc270aa39bc82576
Lahoucine Azmoud <lazmoud@e2r6p6.1337.ma>
2025-04-17 17:08:00 +0100

    Now readline is not echoing shit when the input is not coming from a terminal

--- libft/ft_readline.c
+++ libft/ft_readline.c
@@ -9,17 +9,42 @@
 /*   Updated: 2025/03/25 20:15:59 by mbouhia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <libft.h>
+
+static void	switch_echo_state(int a)
+{
+	static int	stdout_backup;
+	int			dev_null;
+
+	stdout_backup = -1;
+	dev_null = -1;
+	if (isatty(STDIN_FILENO))
+		return ;
+	if (a == SET_NO_ECHO)
+	{
+		stdout_backup = dup(STDOUT_FILENO);
+		dev_null = open(DEV_NULL, O_WRONLY);
+		if (dev_null != -1)
+		{
+			dup2(dev_null, STDOUT_FILENO);
+			close(dev_null);
+		}
+	}
+	else if (a == RESTORE)
+	{
+		dup2(stdout_backup, STDOUT_FILENO);
+		close(stdout_backup);
+	}
+}
 
 t_string	*ft_readline(const char *prompt)
 {
 	t_string	*line;
 	char		*tmp;
 
+	switch_echo_state(SET_NO_ECHO);
 	tmp = readline(prompt);
-	if (!tmp)
-		return (NULL);
+	switch_echo_state(RESTORE);
 	line = str_construct();
 	str_append(tmp, line);
 	free(tmp);
